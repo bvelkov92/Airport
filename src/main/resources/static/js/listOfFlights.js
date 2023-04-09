1b539def8b51f3595edb5b888b02987b187492bd
Boris Velkov <95984793+bvelkov92@users.noreply.github.com>
2023-04-09 10:56:08 +0300

    Delete src directory

--- src/main/resources/static/js/listOfFlights.js
+++
@@ -1,7 +0,0 @@
-let formList = document.getElementById("values").getAttribute("current");
-
-
-fetch("http://localhost:8000/flights/flight-list")
-
-.then((response)=> response.json())
-.then((body)=> console.log(body) )
